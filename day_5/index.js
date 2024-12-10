8e058b0ea2c5ef6601a9fd052dedb71689a728a9
sir-hector <krauskarol7@gmail.com>
2024-12-10 15:50:32 +0100

    day5_part2

--- day_5/index.js
+++ day_5/index.js
@@ -21,6 +21,41 @@ var sum2 = 0;
 // iterate through updates
 sequences.forEach(function (sequence) {
     var sequenceItems = sequence.split(',');
+    var condition = isSorted(sequence);
+    if (condition) {
+        sum += parseInt((sequenceItems[Math.floor(sequenceItems.length / 2)]));
+    }
+    else {
+        // PART 2 prepare mapping that has only values from sequence items
+        var dictionary_1 = {};
+        var ordered = [];
+        sequenceItems.forEach(function (value) {
+            dictionary_1[value] = rules.filter(function (rule) { return rule[0] === value; }).map(function (rule) { return rule[1]; }).filter(function (rule) { return sequenceItems.includes(rule); });
+        });
+        var _loop_1 = function () {
+            // find the key with empty array
+            var emptyKey = Object.keys(dictionary_1).find(function (key) { return dictionary_1[key].length === 0; });
+            //remove the key from all avalues
+            for (var key in dictionary_1) {
+                dictionary_1[key] = dictionary_1[key].filter(function (item) { return item !== emptyKey; });
+            }
+            if (emptyKey) {
+                delete dictionary_1[emptyKey];
+                ordered.unshift(emptyKey);
+            }
+        };
+        while (Object.keys(dictionary_1).length) {
+            _loop_1();
+        }
+        console.log(ordered);
+        console.log((ordered[Math.floor(ordered.length / 2)]));
+        sum2 += parseInt((ordered[Math.floor(ordered.length / 2)]));
+    }
+});
+console.log(sum);
+console.log(sum2);
+function isSorted(sequence) {
+    var sequenceItems = sequence.split(',');
     var condition = true;
     sequenceItems.some(function (value, index) {
         // get values from mapper.
@@ -39,40 +74,5 @@ sequences.forEach(function (sequence) {
         });
         return !condition;
     });
-    if (condition) {
-        sum += parseInt((sequenceItems[Math.floor(sequenceItems.length / 2)]));
-    }
-    else {
-        var sorted = sequenceItems.sort(function (a, b) {
-            if (a < b) {
-                return -1;
-            }
-            return 1;
-        });
-        var condition2 = true;
-        console.log(sorted);
-        console.log((sorted[Math.floor(sorted.length / 2)]));
-        sum2 += parseInt((sorted[Math.floor(sorted.length / 2)]));
-        // sorted.some((value, index) => {
-        //     // get values from mapper.
-        //     const values = rullesMap[value] ? rullesMap[value] : []
-        //     // get rest of string -- only right
-        //     const restOfValues = sorted.slice(index + 1, sorted.length)
-        //     // check if rest has all required values -- only right
-        //     restOfValues.some(value => {
-        //         if (values.includes(value)) {
-        //             condition2 = true;
-        //         } else {
-        //             condition2 = false;
-        //             return true;
-        //         }
-        //     })
-        //     return !condition2
-        // })
-        // if (!condition2) {
-        //     sum2 += parseInt((sorted[Math.floor(sorted.length / 2)]))
-        // }
-    }
-});
-console.log(sum);
-console.log(sum2);
+    return condition;
+}
--- day_5/index.ts
+++ day_5/index.ts
@@ -21,6 +21,44 @@ let sum2 = 0;
 // iterate through updates
 sequences.forEach((sequence) => {
     const sequenceItems = sequence.split(',');
+    const condition = isSorted(sequence)
+
+    if (condition) {
+        sum += parseInt((sequenceItems[Math.floor(sequenceItems.length / 2)]))
+    } else {
+        // PART 2 prepare mapping that has only values from sequence items
+        const dictionary = {};
+        const ordered: string[] = [];
+
+        sequenceItems.forEach(value => {
+            dictionary[value] = rules.filter((rule) => rule[0] === value).map(rule => rule[1]).filter(rule => sequenceItems.includes(rule))
+        })
+
+        while (Object.keys(dictionary).length) {
+            // find the key with empty array
+            const emptyKey = Object.keys(dictionary).find((key) => dictionary[key].length === 0);
+
+            //remove the key from all avalues
+            for (const key in dictionary) {
+                dictionary[key] = dictionary[key].filter(item => item !== emptyKey)
+            }
+
+            if (emptyKey) {
+                delete dictionary[emptyKey];
+                ordered.unshift(emptyKey)
+            }
+        }
+        console.log(ordered)
+        console.log((ordered[Math.floor(ordered.length / 2)]))
+        sum2 += parseInt((ordered[Math.floor(ordered.length / 2)]))
+
+    }
+})
+console.log(sum)
+console.log(sum2)
+
+function isSorted(sequence: string) {
+    const sequenceItems = sequence.split(',');
     let condition = true;
 
     sequenceItems.some((value, index) => {
@@ -40,12 +78,5 @@ sequences.forEach((sequence) => {
 
         return !condition
     })
-
-    if (condition) {
-        sum += parseInt((sequenceItems[Math.floor(sequenceItems.length / 2)]))
-    } else {
-        console.log("part2")
-    }
-})
-console.log(sum)
-console.log(sum2)
+    return condition;
+}
