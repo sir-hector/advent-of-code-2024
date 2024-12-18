fe776a123b5931c3fa9baaf7d49de40568b08a0f
sir-hector <krauskarol7@gmail.com>
2024-12-18 19:03:33 +0100

    day9_part2

--- day_9/index.js
+++ day_9/index.js
@@ -1,45 +1,89 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
-var data = (0, fs_1.readFileSync)('./data.txt', 'utf-8');
+var data = (0, fs_1.readFileSync)('./data.txt', 'utf-8').trim();
 var idNumber = 0;
-var splitted = data.split('').map(BigInt);
+var splitted = data.split('').map(Number);
 var unpackedDisk = [];
 splitted.forEach(function (value, index) {
+    var file = [];
     if (index === 0 || index % 2 === 0) {
         for (var j = value; j > 0; j--) {
-            unpackedDisk.push(idNumber.toString());
+            file.push(idNumber.toString());
         }
         idNumber = idNumber + 1;
     }
     //empty space blok
     else {
         for (var j = value; j > 0; j--) {
-            unpackedDisk.push(".");
+            file.push(".");
         }
     }
+    if (file.length) {
+        unpackedDisk.push(file);
+    }
 });
+// part 1
+// unpackedDisk.forEach((value, index) => {
+//     if (value === '.') {
+//         while (true) {
+//             const temp = unpackedDisk.pop();
+//             if (temp === ".") {
+//                 continue;
+//             } else {
+//                 if (temp) {
+//                     unpackedDisk[index] = temp;
+//                 }
+//                 break;
+//             }
+//         }
+//     }
+// })
 console.log(unpackedDisk);
-unpackedDisk.forEach(function (value, index) {
-    if (value === '.') {
-        while (true) {
-            var temp = unpackedDisk.pop();
-            if (temp === ".") {
-                continue;
-            }
-            else {
-                if (temp) {
-                    unpackedDisk[index] = temp;
+// part 2 
+var movedIds = [];
+outer: for (var i = unpackedDisk.length - 1; i >= 0; i--) {
+    // check from the end if it is not dot and not in moved ids
+    if (unpackedDisk[i][0] !== '.' && !movedIds.includes(unpackedDisk[i][0])) {
+        // then check from the start and check if its dot and length is enough
+        for (var j = 0; j <= i; j++) {
+            if (unpackedDisk[j][0] === '.' && unpackedDisk[j].length >= unpackedDisk[i].length) {
+                // then if the same length replace
+                // else
+                // first replace then dots
+                if (unpackedDisk[j].length === unpackedDisk[i].length) {
+                    movedIds.push(unpackedDisk[i][0]);
+                    var tmp = __spreadArray([], unpackedDisk[j], true);
+                    unpackedDisk[j] = unpackedDisk[i];
+                    unpackedDisk[i] = tmp;
+                    continue outer;
                 }
-                break;
+                else {
+                    movedIds.push(unpackedDisk[i][0]);
+                    var temp = __spreadArray([], unpackedDisk[i], true);
+                    unpackedDisk[i].fill('.');
+                    unpackedDisk.splice(j, 1, temp, unpackedDisk[j].slice(unpackedDisk[i].length));
+                }
             }
         }
     }
-});
+}
+// flat the array
+var defrag = unpackedDisk.flat();
 // calculate check sum
 var checkSum = 0;
-unpackedDisk.forEach(function (value, id) {
-    checkSum += parseInt(value) * id;
+defrag.forEach(function (value, id) {
+    if (value !== '.') {
+        checkSum += parseInt(value) * id;
+    }
 });
-console.log(unpackedDisk);
 console.log(checkSum);
--- day_9/index.ts
+++ day_9/index.ts
@@ -1,50 +1,90 @@
 import { readFileSync } from 'fs';
 
-const data = readFileSync('./data.txt', 'utf-8');
+const data = readFileSync('./data.txt', 'utf-8').trim();
 let idNumber = 0;
 
 const splitted = data.split('').map(Number);
-const unpackedDisk: string[] = []
+const unpackedDisk: string[][] = []
 
 splitted.forEach((value, index) => {
+    const file: string[] = [];
     if (index === 0 || index % 2 === 0) {
         for (let j = value; j > 0; j--) {
-            unpackedDisk.push(idNumber.toString())
+            file.push(idNumber.toString())
         }
         idNumber = idNumber + 1;
     }
     //empty space blok
     else {
         for (let j = value; j > 0; j--) {
-            unpackedDisk.push(".")
+            file.push(".")
         }
     }
+    if (file.length) {
+        unpackedDisk.push(file)
+    }
 })
+
+// part 1
+// unpackedDisk.forEach((value, index) => {
+//     if (value === '.') {
+//         while (true) {
+//             const temp = unpackedDisk.pop();
+//             if (temp === ".") {
+//                 continue;
+//             } else {
+//                 if (temp) {
+//                     unpackedDisk[index] = temp;
+//                 }
+//                 break;
+//             }
+
+//         }
+//     }
+// })
 
 console.log(unpackedDisk)
-unpackedDisk.forEach((value, index) => {
-    if (value === '.') {
-        while (true) {
-            const temp = unpackedDisk.pop();
-            if (temp === ".") {
-                continue;
-            } else {
-                if (temp) {
-                    unpackedDisk[index] = temp;
-                }
-                break;
-            }
 
+// part 2 
+const movedIds: string[] = [];
+outer: for (let i = unpackedDisk.length - 1; i >= 0; i--) {
+    // check from the end if it is not dot and not in moved ids
+    if (unpackedDisk[i][0] !== '.' && !movedIds.includes(unpackedDisk[i][0])) {
+        // then check from the start and check if its dot and length is enough
+        for (let j = 0; j <= i; j++) {
+            if (unpackedDisk[j][0] === '.' && unpackedDisk[j].length >= unpackedDisk[i].length) {
+                // then if the same length replace
+                // else
+                // first replace then dots
+                if (unpackedDisk[j].length === unpackedDisk[i].length) {
+                    movedIds.push(unpackedDisk[i][0]);
+                    let tmp = [...unpackedDisk[j]];
+                    unpackedDisk[j] = unpackedDisk[i]
+                    unpackedDisk[i] = tmp;
+                    continue outer;
+                } else {
+                    movedIds.push(unpackedDisk[i][0]);
+                    const temp = [...unpackedDisk[i]];
+                    unpackedDisk[i].fill('.');
+                    unpackedDisk.splice(j, 1, temp, unpackedDisk[j].slice(unpackedDisk[i].length))
+                }
+            }
         }
     }
-})
+}
+
+
+// flat the array
+
+const defrag = unpackedDisk.flat();
 
 
 // calculate check sum
 let checkSum = 0;
-unpackedDisk.forEach((value, id) => {
-    checkSum += parseInt(value) * id
+defrag.forEach((value, id) => {
+    if (value !== '.') {
+        checkSum += parseInt(value) * id
+    }
 })
 
-console.log(unpackedDisk)
 console.log(checkSum)
